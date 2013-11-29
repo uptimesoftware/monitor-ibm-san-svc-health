268105a899ba2880bacc3d9a3e19764062d47fa3
Patrick Lui <patrick.lui@uptimesoftware.com>
2013-11-29 15:50:39 -0500

    Controller include/exclude works

--- pkg/files/scripts/Monitor_IBM_SAN_SVC/SVC_monitor.php
+++ pkg/files/scripts/Monitor_IBM_SAN_SVC/SVC_monitor.php
@@ -1,17 +1,71 @@
 <?php
-    $user=getenv('UPTIME_CIM-USERNAME');	
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    
+	// Operational Status - array 
+	//		Unknown 0						Stopping 9
+	//		Other 1 						Stopped 10
+	//		OK 2 							In Service 11
+	//		Degraded 3 						No Contact 12
+	//		Stressed 4 						Lost Communication 13
+	//		Predictive Failure 5 			Aborted 14
+	//		Error 6 						Dormant 15
+	//		Non-Recoverable Error 7 		Supporting Entity in Error 16	
+	//		Starting 8         				Completed 17 
+	//										Power Mode 18 
+	
+	$ok_op_status = array(2,11,17);  
+	$warning_op_status = array(1,3,4,5,8,9,14,15,18);  
+	$critical_op_status = array(6,7,10,12,13,16);  
+	$unknown_op_status = array(0);
+	$op_status = array('Unknown','Other','OK','Degraded','Stressed','Predictive Failure','Error','Non-Recoverable Error','Starting','Stopping','Stopped','In Service','No Contact','Lost Communication','Aborted','Dormant','Supporting Entity in Error','Completed','Power Mode'); 
+	
+	// Native Status 
+	// 		Offline 0 
+	//		Online 1 
+	//		Pending 2 
+	//		Adding 3 
+	//		Deleting 4 
+	//		Flushing 5
+	
+	$ok_nat_status = array(1,5);
+	$warning_nat_status = array(2,3,4);
+	$critical_nat_status = array(0);
+	
+	
+	// Get Input Variables
+	$user=getenv('UPTIME_CIM-USERNAME');	
     $password=getenv('UPTIME_CIM-PASSWORD');
     $ip=getenv('UPTIME_HOSTNAME');
 	$port=getenv('UPTIME_CIM-PORT');
-
-    if($user !='' && $password!='' && $ip!=''){    
+	
+	$controllerFilterString=getenv('UPTIME_CONTROLLERFILTERSTRING');
+	//None|Include|Exclude
+	$controllerStatusFilter=getenv('UPTIME_CONTROLLERFILTEROPTION');
+	//true|false
+	$controllerStatusCheck=getenv('UPTIME_CONTROLLERSTATUSCHECK');
+	
+	
+	
+    if($user !='' && $password!='' && $ip!=''){
 	
 		// Backend Controller
         $cmd_str ="https://$user:$password@$ip:$port/root/ibm:IBMTSSVC_BackendController";    
         
-        $result=shell_exec("wbemcli -noverify ei '$cmd_str'");    
+        //$result=shell_exec("wbemcli -noverify ei '$cmd_str'");    
+        $result=shell_exec("type IBMTSSVC_BackendController.txt");    
+		$controllerAlert = array();
+		if ($controllerStatusFilter == "Include") {
+			$tmpIncludeList = explode(',', $controllerFilterString);
+			$includeList = array_map('trim', $tmpIncludeList);
+		}
+		elseif($controllerStatusFilter == "Exclude") {
+			$tmpExludeList = explode(',', $controllerFilterString);
+			$excludeList = array_map('trim', $tmpExludeList);
+		}
+		
         $skuList = explode("\n",$result); 
-        $op_status = array('','Other','OK','Degraded','Stressed','Predictive Failure','Error','Non-Recoverable Error','Starting','Stopping','Stopped','In Service','No Contact','Lost Communication','Aborted','Dormant','Supporting Entity in Error','Completed','Power Mode');  
+ 
+		// Looping through each controller
         foreach ($skuList as $sys_each_value){
 			if (!empty($sys_each_value)) {
 				$num_sys= explode(',',$sys_each_value);
@@ -21,7 +75,7 @@
 						$controller = explode('=',$value);
 						$bc_name = str_replace('"', '', $controller[1]);
 					} 
-					if(preg_match('/^OperationalStatus/i', trim($value))){               
+					if(preg_match('/^OperationalStatus/i', trim($value))){
 						$operation = explode('=',$value);
 						if($operation[1] <19 && $operation[1] != '..' && $operation[1] != '0x8000..'){
 							$opst  = $op_status[$operation[1]];
@@ -32,17 +86,49 @@
 						if($operation[1] == '0x8000..'){
 							$opst  ='Vendor Reserved';                    
 						} 
-					
 					}           
 				}
-				//echo $bc_name.".BCOperationalStatus ".$opst."\n";
-				echo $bc_name.".BCOperationalStatus ".$operation[1]."\n";
+				
+				
+				// No Filter
+				if($controllerStatusFilter == "None") {
+					echo $bc_name.".BCOperationalStatus ".$operation[1]."\n";
+					$controllerAlert = checkOpStatus($bc_name,$operation[1],$op_status,$controllerAlert,$critical_op_status,$warning_op_status,$unknown_op_status);
+				} 
+				elseif ($controllerStatusFilter == "Include") {
+
+					if (in_array($bc_name, $includeList)) {
+						echo $bc_name.".BCOperationalStatus ".$operation[1]."\n";
+
+						$controllerAlert = checkOpStatus($bc_name,$operation[1],$op_status,$controllerAlert,$critical_op_status,$warning_op_status,$unknown_op_status);
+						
+						// Remove value from array so we know what we haven't found yet
+						$indexFound = array_search($bc_name, $includeList);
+						unset($includeList[$indexFound]);
+						$includeList = array_values($includeList);
+						
+					}
+				}
+				elseif ($controllerStatusFilter == "Exclude") {
+					if (!in_array($bc_name, $excludeList)) {
+						echo $bc_name.".BCOperationalStatus ".$operation[1]."\n";
+						$controllerAlert = checkOpStatus($bc_name,$operation[1],$op_status,$controllerAlert,$critical_op_status,$warning_op_status,$unknown_op_status);
+					}
+				}
 			}
         }
+		// If the include list is not empty, this means we didn't find a specified item in the include list
+		// Therefore, exit with error.
+		if (!empty($includeList)) {
+			$comma_separated = implode(",", $includeList);
+			echo "The following controller wasn't found but was specified in the include filter: ". $comma_separated;
+			exit(2);
+		}
+		
+		
 		
 		// Backend Volume 
 		$cmd_str ="https://$user:$password@$ip:$port/root/ibm:IBMTSSVC_BackendVolume";    
-        //$result=shell_exec("wbemcli ei -nl '$cmd_str'");    
         $result=shell_exec("wbemcli -noverify ei '$cmd_str'");    
 
         $skuList = explode("\n",$result); 
@@ -69,7 +155,6 @@
 						} 
 					}
 				}
-				//echo $bc_name.".BVOperationalStatus ".$opst."\n";
 				echo $bc_name.".BVOperationalStatus ".$operation[1]."\n";
 			}
         }
@@ -165,7 +250,7 @@
 						$pool = explode('=',$value);
 						$elementName = str_replace('"', '', $pool[1]);
 					}
-					if(preg_match('/^OperationalStatus/i', trim($value))){               
+					if(preg_match('/^OperationalStatus/i', trim($value))){
 						$operation = explode('=',$value);
 						if($operation[1] <19 && $operation[1] != '..' && $operation[1] != '0x8000..'){
 							$opst  = $op_status[$operation[1]];
@@ -176,7 +261,7 @@
 						if($operation[1] == '0x8000..'){
 							$opst  ='Vendor Reserved';                    
 						} 
-					} 
+					}
 				}
 
 				//echo $elementName.".VDOperationalStatus ".$opst."\n";
@@ -220,9 +305,22 @@
 					if(preg_match('/^Caption/i', trim($value))){              
 						$caption = explode('=',$value);
 						$apps_temp = str_replace('"', '', $caption[1]);              
-					} 
+					}
+					if(preg_match('/^OperationalStatus/i', trim($value))){
+						$operation = explode('=',$value);
+						if($operation[1] <19 && $operation[1] != '..' && $operation[1] != '0x8000..'){
+							$opst  = $op_status[$operation[1]];
+						}
+						if($operation[1] == '..'){                    
+							$opst  ='DMTF Reserved';                    
+						}
+						if($operation[1] == '0x8000..'){
+							$opst  ='Vendor Reserved';                    
+						} 
+					}
 				}
-				//echo "$output\n\n";
+				
+				echo $apps_temp.".SPOperationalStatus ".$operation[1]."\n";
 				echo $apps_temp.".SPOverall ".$overall_allocation."\n";
 				echo $apps_temp.".SPTotal ".$total_managed_space."\n";
 				echo $apps_temp.".SPRemaining ".$remaining_managed_space."\n";
@@ -307,10 +405,47 @@
 
 			}
 		}
+
+		$highest_severity = 0;
+		// Go through all the alert arrays and exit the script accordingly
+		// Each item is an array where...
+		// [0] - name
+		// [1] - severity (i.e. 1 = Warning, 2 = Critical, 3 = Unknown)
+		// [2] - status description
+		foreach ($controllerAlert as $item) {
+			if ($item[1] > $highest_severity ) {
+				$highest_severity = $item[1];
+			}
+			echo $item[0] . " is in " . $item[2] . " state.\n";			
+		}
+		exit($highest_severity);
+
 		
 		
 		
     }else{    
         echo "please enter the correct parameter Ex:- php xxxx.php admin admin_pass 000.000.000.000\n";
     }
+	
+	function checkOpStatus($name,$status,$status_text_array,$alertArray,$critical_array,$warning_array,$unknown_array) {
+	
+		if (in_array($status, $critical_array)) {
+			$alertArray[] =  array($name, 2, $status_text_array[$status]);		
+			return $alertArray;
+		}
+		elseif (in_array($status, $warning_array)) {
+			$alertArray[] =  array($name, 1, $status_text_array[$status]);		
+			return $alertArray;
+		}
+		elseif (in_array($status, $unknown_array)) {
+			$alertArray[] =  array($name, 3, $status_text_array[$status]);		
+			return $alertArray;
+		} 
+		else {
+			return;
+		}
+	
+	}
+
+
 ?>
